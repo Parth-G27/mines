a7190e2fb109aaee9e0b6649b2d717b0620d9758
Parth <parthbidari13@gmail.com>
2024-09-13 00:36:22 +0530

    Stop tracking .env file in clinet

--- client/app/api/auth/[...nextauth]/route.jsx
+++ client/app/api/auth/[...nextauth]/route.jsx
@@ -1,10 +1,10 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 // provider is provider by which I have signed-in the user into my application
 
 export const authOptions = {
   providers: [
-    GithubProvider({
+    GoogleProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
